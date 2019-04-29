4ead3519acf6c79b2e6cd5f8989d0455f51aa7c6
Thomas Shen <Tom752199526@outlook.com>
2019-04-29 16:21:02 -0500

    Some update.

--- othello.py
+++ othello.py
@@ -5,6 +5,7 @@
 import turtle
 import math
 import random
+import numpy
 
 #draw green square
 def square(length):
@@ -183,9 +184,8 @@ def getvalidMoves(board,color):
 ### selectNewplay computer's funtion
 def selectNextPlay(board):
     alist = getvalidMoves(board,1)
-    a = random.randint(0,len(alist)-1)
-    x = alist[a][0]
-    y = alist[a][1]
+    new_board = board
+    [x, y] = MinimaxDecision(new_board, alist, 1)
     board[x][y] = 1
     move(x,y,board)
     flip(x,y,board,1)
@@ -201,6 +201,48 @@ def count(board):
             elif board[i][j] == -1:
                 black += 1
     return white, black
+
+## Given a state(current board) in a game, calculate the best move by searching
+## forward all the way to the terminal states.
+def MinimaxDecision(board, valid_moves, player = 1):
+
+    argmax = max
+    infinity = math.inf
+
+    def MaxValue(board, the_move, opponent):
+        if len(valid_moves) == 0 :
+            (black, white) = count(board)
+            return (white - black)
+        v = -infinity
+        [x, y] = the_move
+        new_board = board
+        move(x, y, new_board)
+        flip(x, y, new_board, opponent)
+        next_valid_moves = getvalidMoves(board, -opponent)
+        for next in next_valid_moves :
+            v = max(v, MinValue(board, next, -opponent))
+        return v
+
+    def MinValue(board, the_move, opponent):
+        if len(valid_moves) == 0 :
+            (black, white) = count(board)
+            return (white - black)
+        v = infinity
+        [x, y] = the_move
+        move(x, y, board)
+        flip(x, y, board, 1)
+        next_valid_moves = getvalidMoves(board, -opponent)
+        for next in next_valid_moves :
+            v = min(v, MaxValue(board, next, opponent))
+        return v
+
+    # Body of minimax_decision:
+    max_value = -infinity
+    best_choice = []
+    for next in valid_moves:
+        if max_value < MinValue(board, next, 1):
+            best_choice = next
+    return best_choice
 
 ###decision function
 def desicion(white,black):
