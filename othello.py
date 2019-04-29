8c561577183550495c3387848710f3609b12ec6e
Thomas Shen <Tom752199526@outlook.com>
2019-04-29 16:44:16 -0500

    Update.

--- othello.py
+++ othello.py
@@ -210,37 +210,44 @@ def MinimaxDecision(board, valid_moves, player = 1):
     infinity = math.inf
 
     def MaxValue(board, the_move, opponent):
+        # Final state check
         if len(valid_moves) == 0 :
             (black, white) = count(board)
             return (white - black)
+        # Current player move
         v = -infinity
         [x, y] = the_move
         new_board = board
-        move(x, y, new_board)
         flip(x, y, new_board, opponent)
-        next_valid_moves = getvalidMoves(board, -opponent)
+        # Next movement from opponent
+        next_valid_moves = getvalidMoves(new_board, -opponent)
         for next in next_valid_moves :
-            v = max(v, MinValue(board, next, -opponent))
+            v = max(v, MinValue(new_board, next, -opponent))
         return v
 
     def MinValue(board, the_move, opponent):
+        # Final state check
         if len(valid_moves) == 0 :
             (black, white) = count(board)
             return (white - black)
+        # Current player move
         v = infinity
         [x, y] = the_move
-        move(x, y, board)
-        flip(x, y, board, 1)
-        next_valid_moves = getvalidMoves(board, -opponent)
+        new_board = board
+        flip(x, y, new_board, opponent)
+        # Next movement from opponent
+        next_valid_moves = getvalidMoves(new_board, -opponent)
         for next in next_valid_moves :
-            v = min(v, MaxValue(board, next, opponent))
+            v = min(v, MaxValue(new_board, next, -opponent))
         return v
 
     # Body of minimax_decision:
     max_value = -infinity
     best_choice = []
     for next in valid_moves:
-        if max_value < MinValue(board, next, 1):
+        new_value = MinValue(board, next, player)
+        if max_value < new_value:
+            max_value = new_value
             best_choice = next
     return best_choice
 
