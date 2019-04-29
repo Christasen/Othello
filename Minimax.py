7660f41b97aea5883425f5291c47d499bdc40288
Thomas Shen <Tom752199526@outlook.com>
2019-04-29 17:51:09 -0500

    ..

--- Minimax.py
+++ Minimax.py
@@ -1,6 +1,40 @@
 import numpy
 import math
 
+def oppon(row, col, board, color):
+    oppon1 = []
+    for u in [-1,0,1]:
+        for v in [-1,0,1]:
+            if row + u >= 0 and col + v >= 0 and row + u < len(board) and col + v < len(board)\
+            and board[row+u][col+v] == -color:
+                oppon1.append([u,v])
+    return oppon1
+
+def flip(move, board, color):
+    [row, col] = move
+    oppon3 = []
+    oppon3 = oppon(row,col,board,color)
+    for el in oppon3:
+        u = el[0]
+        v = el[1]
+        r = row + u
+        c = col + v
+        flip1 = [] ###@@@@
+        while r >= 0 and c >= 0 and r < len(board) and c < len(board) and board[r][c] == -color:
+            r += u
+            c += v
+            if r >= 0 and c >= 0 and r < len(board) and c < len(board):
+                if board[r][c] == color:
+                    flip1.append([r,c])
+        if flip1 != []:
+            r -= u
+            c -= v
+            while r != row or c!= col:
+                board[r][c] = color
+                r -= u
+                c -= v
+    return board
+
 ## Given a state(current board) in a game, calculate the best move by searching
 ## forward all the way to the terminal states.
 def MinimaxDecision(board, valid_moves, player = 1):
@@ -8,44 +42,40 @@ def MinimaxDecision(board, valid_moves, player = 1):
     argmax = max
     infinity = math.inf
 
-    def MaxValue(board, the_move, opponent):
+    def MaxValue(board, the_move, opponent, depth):
         # Final state check
-        if len(valid_moves) == 0 :
+        if len(valid_moves) == 0 or depth == 5:
             (black, white) = count(board)
             return (white - black)
         # Current player move
         v = -infinity
-        [x, y] = the_move
-        new_board = board
-        flip(x, y, new_board, opponent)
+        new_board = flip(the_move, board, opponent)
         # Next movement from opponent
         next_valid_moves = getvalidMoves(new_board, -opponent)
         for next in next_valid_moves :
-            v = max(v, MinValue(new_board, next, -opponent))
+            v = max(v, MinValue(new_board, next, -opponent, depth + 1))
         return v
 
-    def MinValue(board, the_move, opponent):
+    def MinValue(board, the_move, opponent, depth):
         # Final state check
-        if len(valid_moves) == 0 :
+        if len(valid_moves) == 0 or depth == 5:
             (black, white) = count(board)
             return (white - black)
         # Current player move
         v = infinity
-        [x, y] = the_move
-        new_board = board
-        flip(x, y, new_board, opponent)
+        new_board = flip(the_move, board, opponent)
         # Next movement from opponent
         next_valid_moves = getvalidMoves(new_board, -opponent)
         for next in next_valid_moves :
-            v = min(v, MaxValue(new_board, next, -opponent))
+            v = min(v, MaxValue(new_board, next, -opponent, depth + 1))
         return v
 
     # Body of minimax_decision:
-    max_value = -infinity
-    best_choice = []
+    best_score = -infinity
+    best_choice = None
     for next in valid_moves:
-        new_value = MinValue(board, next, player)
-        if max_value < new_value:
-            max_value = new_value
+        score = MinValue(board, next, player, 1)
+        if best_score < score:
+            best_score = score
             best_choice = next
     return best_choice
--- othello.py
+++ othello.py
@@ -6,6 +6,7 @@ import turtle
 import math
 import random
 import numpy
+from Minimax import MinimaxDecision
 
 #draw green square
 def square(length):
@@ -201,55 +202,6 @@ def count(board):
             elif board[i][j] == -1:
                 black += 1
     return white, black
-
-## Given a state(current board) in a game, calculate the best move by searching
-## forward all the way to the terminal states.
-def MinimaxDecision(board, valid_moves, player = 1):
-
-    argmax = max
-    infinity = math.inf
-
-    def MaxValue(board, the_move, opponent):
-        # Final state check
-        if len(valid_moves) == 0 :
-            (black, white) = count(board)
-            return (white - black)
-        # Current player move
-        v = -infinity
-        [x, y] = the_move
-        new_board = board
-        flip(x, y, new_board, opponent)
-        # Next movement from opponent
-        next_valid_moves = getvalidMoves(new_board, -opponent)
-        for next in next_valid_moves :
-            v = max(v, MinValue(new_board, next, -opponent))
-        return v
-
-    def MinValue(board, the_move, opponent):
-        # Final state check
-        if len(valid_moves) == 0 :
-            (black, white) = count(board)
-            return (white - black)
-        # Current player move
-        v = infinity
-        [x, y] = the_move
-        new_board = board
-        flip(x, y, new_board, opponent)
-        # Next movement from opponent
-        next_valid_moves = getvalidMoves(new_board, -opponent)
-        for next in next_valid_moves :
-            v = min(v, MaxValue(new_board, next, -opponent))
-        return v
-
-    # Body of minimax_decision:
-    max_value = -infinity
-    best_choice = []
-    for next in valid_moves:
-        new_value = MinValue(board, next, player)
-        if max_value < new_value:
-            max_value = new_value
-            best_choice = next
-    return best_choice
 
 ###decision function
 def desicion(white,black):
